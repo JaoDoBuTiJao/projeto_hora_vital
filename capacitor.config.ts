78caf734e36e8f9f20c28d83632837dc59e7d381
JaoDoBuTiJao <victor.joao.ss27@gmail.com>
2025-09-30 19:23:36 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projeto_hora_vital',
+  webDir: 'www'
+};
+
+export default config;
